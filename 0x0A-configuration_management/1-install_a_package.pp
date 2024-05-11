854cfafda4a296197bc00adac466f4c1fb89b9af
Gift Aterezi <giftaterezi@gmail.com>
2024-05-11 04:26:15 -0700

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,13 @@
 #!/usr/bin/puppet
-#
-#:wq
-#
+
+# Install a specific version of Flask (2.1.0) using pip3
+package { 'python3-flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'], # Ensure pip3 is installed first
+}
+
+# Install pip3 if not already installed
+package { 'python3-pip':
+  ensure => installed,
+}
