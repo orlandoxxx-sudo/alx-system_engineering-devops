fc43c7cc865158281dd959d068c69b6b2aaf9539
orlandoxxx-sudo <138414238+orlandoxxx-sudo@users.noreply.github.com>
2024-06-05 05:22:25 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,12 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    
+    if response.status_code == 200:
+        try:
+            results = response.json().get("data")
+            return results.get("subscribers", 0)
+        except (ValueError, KeyError, TypeError):
+            return 0
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
