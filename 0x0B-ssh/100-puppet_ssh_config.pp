0fff73fe1acfcb8a6523956afc742f6eb56e079d
orlandogift-sudo <orlandogiftxxx@gmail.com>
2024-06-15 11:28:29 -0700

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 # ussing puppet command 
+include stdlib
 
-file { 'ect/ssh/ssh_config':
-	ensure => present,
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
 
-content =>"
-	#SSH client configuration
-	host*
-	IdentifyFile ~/.ssh/school
-	PasswordAuthentication no
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
